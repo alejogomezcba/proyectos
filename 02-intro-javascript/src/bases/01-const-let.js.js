d42777b3bf1fcebcee31ec5fb082655984fe5cb5
alejogomezcba <alejorgomez@mercadolibre.com>
2022-07-05 17:40:38 -0300

    Subiendo archivos

---
+++ 02-intro-javascript/src/bases/01-const-let.js.js
@@ -0,0 +1,13 @@
+const nombre = "Alejo";
+const apellido = "Gomez";
+let valorDado = 6;
+valorDado = 2;
+
+console.log(`Primer log con nombre y apellido= ${nombre} ${apellido}`);
+
+if (true){
+const nombre = "León";
+console.log(`Segundo log con nombre= ${nombre}`);
+}
+
+console.log(valorDado);
