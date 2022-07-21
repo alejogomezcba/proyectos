2f88d69ec986622e141a456cd7e26e8189c6cd9e
alejogomezcba <alejorgomez@mercadolibre.com>
2022-07-21 11:09:11 -0300

    test

---
+++ 03-counter-app-vite/test2/demo.test.js
@@ -0,0 +1,7 @@
+describe('Testing demo component', () => {
+    test('Esta prueba no debe fallar', () => {
+        if(0===1){
+            throw new Error('No se puede dividir');
+        }
+        })
+})
